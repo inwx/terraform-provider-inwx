1279d9637b246cbc1bf4bc476c664d6309519a77
Felix Soederhuyzen <fsoederhuyzen@inwx.de>
2023-02-13 11:55:27 +0100

    feat: add debug configuration

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"flag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/inwx/terraform-provider-inwx/inwx"
 )
 
+// Provider documentation generation.
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-name hashicups
+
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{
+		Debug: debug,
 		ProviderFunc: func() *schema.Provider {
 			return inwx.Provider()
 		},
-	})
+	}
+
+	plugin.Serve(opts)
 }
