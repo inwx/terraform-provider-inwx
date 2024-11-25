2fc950a72a025c8920a00cd5818dc27356be74e8
Felix Soederhuyzen <fsoederhuyzen@inwx.de>
2024-11-25 11:08:25 +0100

    chore: remove old v5 provider

--- main.go
+++ main.go
@@ -4,12 +4,7 @@ import (
 	"context"
 	"flag"
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
-	"github.com/hashicorp/terraform-plugin-go/tfprotov6/tf6server"
-	"github.com/hashicorp/terraform-plugin-mux/tf5to6server"
-	"github.com/hashicorp/terraform-plugin-mux/tf6muxserver"
 	"github.com/inwx/terraform-provider-inwx/internal/provider"
-	"github.com/inwx/terraform-provider-inwx/inwx"
 	"log"
 )
 
@@ -26,47 +21,19 @@ var (
 )
 
 func main() {
-	ctx := context.Background()
-
 	var debug bool
 
 	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
 
-	upgradedSdkServer, err := tf5to6server.UpgradeServer(ctx, inwx.Provider().GRPCProvider)
+	serveOpts := providerserver.ServeOpts{
+		Address: "registry.terraform.io/inwx/inwx",
+		Debug:   debug,
+	}
+	err := providerserver.Serve(context.Background(), provider.New(version), serveOpts)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err.Error())
 	}
 
-	providers := []func() tfprotov6.ProviderServer{
-		providerserver.NewProtocol6(
-			provider.New(version)(),
-		),
-		func() tfprotov6.ProviderServer {
-			return upgradedSdkServer
-		},
-	}
-
-	muxServer, err := tf6muxserver.NewMuxServer(ctx, providers...)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var serveOpts []tf6server.ServeOpt
-
-	if debug {
-		serveOpts = append(serveOpts, tf6server.WithManagedDebug())
-	}
-
-	err = tf6server.Serve(
-		"registry.terraform.io/inwx/inwx",
-		muxServer.ProviderServer,
-		serveOpts...,
-	)
-
-	if err != nil {
-		log.Fatal(err)
-	}
 }
